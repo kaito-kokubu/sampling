76eb92374d2c30fa9539bcb506ca1c3435cda89e
kokubu <veyron362@gmail.com>
2022-02-27 15:35:49 +0900

    add log to csv

--- ina219_example.py
+++ ina219_example.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python
 
 from Subfact_ina219 import INA219
+import datetime
 
 def main():
     ina = INA219()
-    result = ina.getBusVoltage_V()
+    shunt = ina.getShuntVoltage_mV()
+    bus = ina.getBusVoltage_V()
+    current = ina.getCurrent_mA()
 
-    print("Shunt   : %.3f mV" % ina.getShuntVoltage_mV())
-    print("Bus     : %.3f V" % ina.getBusVoltage_V())
-    print("Current : %.3f mA" % ina.getCurrent_mA())
+    print("Shunt   : %.3f mV" % shunt)
+    print("Bus     : %.3f V" % bus)
+    print("Current : %.3f mA" % current)
+
+    return [datetime.datetime.now(), shunt, bus, current]
 
 if __name__ == '__main__':
     main()
--- read_data.py
+++ read_data.py
@@ -1,8 +1,11 @@
 import signal
 import time
+from unittest import result
 from Subfact_ina219 import INA219
 import ina219_example
-import led
+import csv
+import datetime
+#import led
 
 first_exec_time = 1 #1回目の実行までの時間 (s)
 interval = 1 #2回目以降の実行間隔 (s)
@@ -10,8 +13,11 @@ read_data_time = 120 #計測時間 (s)
 
 #led.led_start(read_data_time)
 
+result_data = []
+
 def scheduler(arg1, arg2):
-    ina219_example.main()
+    result = ina219_example.main()
+    result_data.append(result)
 
 #ina = INA219()
 #print(ina)
@@ -19,5 +25,14 @@ def scheduler(arg1, arg2):
 signal.signal(signal.SIGALRM, scheduler)
 signal.setitimer(signal.ITIMER_REAL, first_exec_time, interval)
 
-while True:
-    time.sleep(interval)
+try:
+    while True:
+        time.sleep(interval)
+except KeyboardInterrupt:
+    print("Keyboard Interrupted!")
+    now = datetime.datetime.now()
+    filename = './output/log_' + now.strftime('%Y%m%d_%H%M%S') + '.csv'
+    with open(filename) as f:
+        writer = csv.writer(f)
+        writer.writerows(result_data)
+    print("Data saved!")
