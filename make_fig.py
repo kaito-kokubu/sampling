e52390ab1889e561f81554fbcedd247fb0a4b503
kokubu <veyron362@gmail.com>
2022-02-27 19:42:40 +0900

    add led func

--- make_fig.py
+++ make_fig.py
@@ -1,19 +1,18 @@
 from email import header
 import matplotlib.pyplot as plt
-import csv
 import pandas as pd
 
 def main():
     file_date = input('Enter the date of log file: ')
-    file_path = "/home/pi/Desktop/sampling/output/log_" + file_date + ".csv"
+    file_path = "./output/log_" + file_date + ".csv"
     df = pd.read_csv(file_path, names=('Time', 'Shunt', 'Bus', 'Current'))
     df['Power'] = df['Shunt'] * df['Current']
-
     fig, ax = plt.subplots()
     ax.plot(df['Time'], df['Power'])
     ax.set_xlabel('Time')
     ax.set_ylabel('Power (mW)')
-    fig.savefig(f'/home/pi/Desktop/sampling/output/plot_{file_date}')
+    plt.show()
+    plt.savefig(f"./output/plot_{file_date}.png")
 
 
 
--- read_data.py
+++ read_data.py
@@ -6,6 +6,7 @@ import ina219_example
 import csv
 import datetime
 import os
+import RPi.GPIO as GPIO
 #import led
 
 first_exec_time = 1 #1回目の実行までの時間 (s)
@@ -13,11 +14,22 @@ interval = 0.1 #2回目以降の実行間隔 (s)
 #read_data_time = 120 #計測時間 (s)
 
 #led.led_start(read_data_time)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(25, GPIO.OUT)
 
 result_data = []
+i = 0
+led_on = False
 
 def scheduler(arg1, arg2):
+    if i == 50:
+        GPIO.output(25, GPIO.HIGH)
+        led_on = True
     result = ina219_example.main()
+    if led_on:
+        result.append('on')
+    else:
+        result.append('off')
     result_data.append(result)
 
 #ina = INA219()
@@ -38,3 +50,4 @@ except KeyboardInterrupt:
         writer = csv.writer(f)
         writer.writerows(result_data)
     print("Data saved!")
+    GPIO.output(25, GPIO.LOW)
