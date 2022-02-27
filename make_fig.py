d5a9963745e5ec76d06c6eb5138f19449e18f1cb
kokubu <veyron362@gmail.com>
2022-02-27 19:03:50 +0900

    add keyboard input function

--- make_fig.py
+++ make_fig.py
@@ -4,7 +4,7 @@ import csv
 import pandas as pd
 
 def main():
-    file_date = ""
+    file_date = input('Enter the date of log file: ')
     file_path = "/home/pi/Desktop/sampling/output/log_" + file_date + ".csv"
     df = pd.read_csv(file_path, names=('Time', 'Shunt', 'Bus', 'Current'))
     df['Power'] = df['Shunt'] * df['Current']
