f8068a51fbbcf644e3f9f470a76976f47d8e5e85
kokubu <veyron362@gmail.com>
2022-03-16 15:01:57 +0900

    no message

---
+++ binary_reader.py
@@ -0,0 +1,33 @@
+import os
+import datetime
+
+
+def dfs(dir):
+    if os.path.isfile(dir):
+        print(dir)
+        read_binary(dir)
+        print('\n')
+    else:
+        dir_list = os.listdir(dir)
+        next_dir_list = [os.path.join(dir, f) for f in dir_list]
+        for next_dir in next_dir_list:
+            dfs(next_dir)
+
+        
+def read_binary(file):
+    with open(file) as f:
+        data = f.read()
+        #print(data)
+def main():
+    init_dir = os.path.expanduser('~')
+    os.chdir(init_dir)
+    dir_list = os.listdir(path='.')
+    next_dir_list = [os.path.join(init_dir, f) for f in dir_list]
+    for dir in next_dir_list:
+        dfs(dir)
+
+
+if __name__ == '__main__':
+    print(f"exec time: {datetime.datetime.now()}")
+    main()
+    print(f"exec time: {datetime.datetime.now()}")
