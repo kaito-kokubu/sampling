97f12cb960e335376fe2c9fdf900406a78559da4
kokubu <veyron362@gmail.com>
2022-07-05 16:40:54 +0900

    no message

--- ina219.sh
+++ ina219.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+start_time=`date "+%Y-%m-%d %H:%M:%S"`
+
 ./build/profiler -p >> ./result/ina219_$(date +%Y%m%d%H%M).csv
+
+end_time=`date "+%Y-%m-%d %H:%M:%S"`
+
+echo $start_time
+echo $end_time
