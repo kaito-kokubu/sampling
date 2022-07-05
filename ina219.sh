8627666943a8a01051912bbe4ccb23e1aada53e1
kokubu <veyron362@gmail.com>
2022-07-06 06:08:47 +0900

    no message

--- ina219.sh
+++ ina219.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-date_today=`date +%Y%m%d%H%M`
+echo `date "+%Y-%m-%d %H:%M:%S"`
 
-echo `date "+%Y-%m-%d %H:%M:%S"` >> ./result/ina219_${date_today}.csv
-
-./build/profiler -p >> ./result/ina219_${date_today}.csv
+./build/profiler -p >> ./result/ina219_$(date +%Y%m%d%H%M).csv
